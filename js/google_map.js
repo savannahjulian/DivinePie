f18b9f58acfe457550d42658d7bd592c6efce09e
gomowjames <pdxemail@pdx.edu>
2013-05-19 15:26:43 -0700

    google map work

--- js/google_map.js
+++ js/google_map.js
@@ -1,24 +1,39 @@
 function initialize() {
 
 var styles = [
-	{"featureType": "road.highway",	"elementType": "geometry.fill", "stylers": [ { "visibility": "on" }, { "color": "#c22430" } ] },{ "featureType": "road.local", "elementType": "geometry.fill", "stylers": [ { "color": "#c22430" }, { "lightness": 100 } ] },{ "featureType": "road.local", "elementType": "geometry.stroke", "stylers": [ { "color": "#808080" }, { "weight": 0.4 }, { "visibility": "on" } ] },{ "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" }, { "color": "#c22430" }, { "gamma": 2 } ] },{ "elementType": "labels.text.stroke", "stylers": [ { "visibility": "on" }, { "color": "#f0f0f0" } ] },{ "featureType": "landscape.man_made", "stylers": [ { "visibility": "on" }, { "color": "#f0f0f0" } ] },{ "featureType": "poi", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" }, { "color": "#c22430" }, { "lightness": 70 } ] },{ "featureType": "road.highway", "elementType": "labels.icon", "stylers": [ { "visibility": "on" }, { "lightness": 10 }, { "gamma": 9.91 }, { "invert_lightness": true } ] } ];
+	{ "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [ { "color": "#808080" }, { "visibility": "on" }, { "lightness": 30 } ] },{ "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [ { "visibility": "on" }, { "weight": 0 } ] },{ "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" }, { "color": "#c8c8c8" }, { "lightness": -10 } ] },{ "featureType": "road.arterial", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "simplified" } ] },{ "featureType": "road.highway", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "on" }, { "color": "#ffffff" } ] },{ "featureType": "poi", "elementType": "geometry.fill", "stylers": [ { "color": "#e31f2d" }, { "lightness": 60 }, { "saturation": 60 }, { "gamma": 1.45 }, { "visibility": "off" } ] },{ "featureType": "water", "elementType": "geometry.fill", "stylers": [ { "visibility": "on" }, { "color": "#d2d2d2" }, { "saturation": -100 } ] },{ "featureType": "landscape.natural.terrain", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road.highway", "elementType": "labels.icon", "stylers": [ { "hue": "#ff001a" }, { "saturation": 100 }, { "lightness": 20 }, { "visibility": "simplified" } ] },{ "featureType": "administrative.neighborhood", "elementType": "labels.text.fill", "stylers": [ { "lightness": 30 }, { "color": "#808080" }, { "visibility": "on" }, { "hue": "#ff0000" }, { "weight": 0.1 }, { "saturation": 100 } ] },{ "featureType": "landscape.man_made", "elementType": "geometry.fill", "stylers": [ { "visibility": "simplified" } ] },{ "featureType": "poi" },{ "featureType": "poi", "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "transit", "stylers": [ { "visibility": "on" }, { "hue": "#ff1a00" }, { "lightness": 30 } ] },{ } ];
 
 var styledMap = new google.maps.StyledMapType(styles,
-    {name: "Divine Pie Vendors"});
+    {name: "Divine Pie"});
+
+var myLatLng = new google.maps.LatLng(45.514527, -122.596436);
 
 var mapOptions = {
-    center: new google.maps.LatLng(45.521743, -122.653328),
     zoom: 11,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+    center: myLatLng,
+   	mapTypeId: google.maps.MapTypeId.ROADMAP, styles: styles
+    };
 
-var map = new google.maps.Map(document.getElementById("map-canvas"),
+var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
-}
+
+var kmlLayer = new google.maps.KmlLayer({url: 'DPvendors.kml', suppressInfoWindows: true,
+	preserveViewport: false,
+	map: map
+});
 
 map.mapTypes.set('map_style', styledMap);
 
 map.setMapTypeId('map_style');
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addListener(kmlLayer, 'click', function(kmlEvent) {
+var text = kmlEvent.featureData.description;
+showInContentWindow(text);
+});
 
+function showInContentWindow(text) {
+var sidediv = document.getElementById('content-window');
+sidediv.innerHTML = text;
+}
+};
+google.maps.event.addDomListener(window, 'load', initialize);
